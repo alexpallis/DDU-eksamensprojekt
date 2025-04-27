4f16789ee5d5105ccde6cea466712596ee8f61ce
114234-Kiwi <krwi1@elev.tec.dk>
2025-04-27 14:31:05 +0200

    Moved

--- eksamensprojekt-ddu/Scripts/additem.gd
+++ eksamensprojekt-ddu/Scripts/additem.gd
@@ -6,13 +6,13 @@ var currdavid
 
 
 func _on_pressed():
+	@warning_ignore("shadowed_variable")
 	var currdavid = str(randi() % 11)
 	owner.find_child("Inventoria").add_slot(currdavid)
 
 var randonumb = currdavid
 
-func _on_additem_pressed() -> void:
-	pass # Replace with function body.
+func _process(delta: float):
 	if randonumb == 0:
 			Global.Dave += 1
 	if randonumb == 2:
@@ -35,36 +35,36 @@ func _on_additem_pressed() -> void:
 		Global.D9 += 1
 	if randonumb == 11:
 		Global.D10 += 1
-	#if randonumb == 12:
-		#Global.D11 += 1
-	#if randonumb == 13:
-		#Global.D12 += 1
-	#if randonumb == 14:
-		#Global.D13 += 1
-	#if randonumb == 15:
-		#Global.D14 += 1
-	#if randonumb == 16:
-		#Global.D15 += 1
-	#if randonumb == 17:
-		#Global.D16 += 1
-	#if randonumb == 18:
-		#Global.D17 += 1
-	#if randonumb == 19:
-		#Global.D18 += 1
-	#if randonumb == 20:
-		#Global.D19 += 1
-	#if randonumb == 21:
-		#Global.D20 += 1
-	#if randonumb == 22:
-		#Global.D21 += 1
-	#if randonumb == 23:
-		#Global.D22 += 1
-	#if randonumb == 24:
-		#Global.D23 += 1
-	#if randonumb == 25:
-		#Global.D24 += 1
-	#if randonumb == 26:
-		#Global.D25 += 1
+	if randonumb == 12:
+		Global.D11 += 1
+	if randonumb == 13:
+		Global.D12 += 1
+	if randonumb == 14:
+		Global.D13 += 1
+	if randonumb == 15:
+		Global.D14 += 1
+	if randonumb == 16:
+		Global.D15 += 1
+	if randonumb == 17:
+		Global.D16 += 1
+	if randonumb == 18:
+		Global.D17 += 1
+	if randonumb == 19:
+		Global.D18 += 1
+	if randonumb == 20:
+		Global.D19 += 1
+	if randonumb == 21:
+		Global.D20 += 1
+	if randonumb == 22:
+		Global.D21 += 1
+	if randonumb == 23:
+		Global.D22 += 1
+	if randonumb == 24:
+		Global.D23 += 1
+	if randonumb == 25:
+		Global.D24 += 1
+	if randonumb == 26:
+		Global.D25 += 1
 	#if randonumb == 27:
 		#Global.D26 += 1
 	#if randonumb == 28:
@@ -95,3 +95,5 @@ func _on_additem_pressed() -> void:
 		#Global.D39 += 1
 	#if randonumb == 41:
 		#Global.D40 += 1
+	print(currdavid , Global.Dave + Global.D1 + Global.D2 + Global.D3 + Global.D4 + Global.D5 + Global.D6 + Global.D7 + Global.D8 + Global.D9 + Global.D10 + Global.D11)
+	
